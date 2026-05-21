ed08fee1089f64dfe4eddd9cfde4685d06093871
Lung_phage <liuzifeng19990216@stu.jxau.edu.cn>
2026-05-21 07:42:10 -0600

    init: 流光镜影博客 - Astro + Decap CMS

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '流光镜影';
+export const SITE_DESCRIPTION = '记录光影与瞬间的故事';
